501794a8323250b69ba6987bfb52ae2e8d959881
Shishir Jha <shishir230501@gmail.com>
2023-11-08 10:26:00 +0530

    UI updated

--- client/src/pages/user/Dashboard.jsx
+++ client/src/pages/user/Dashboard.jsx
@@ -1,10 +1,26 @@
 import React from "react";
 import Layout from "../../components/Layout/Layout";
+import UserMenu from "../../components/Layout/UserMenu";
+import { useAuth } from "../../context/auth";
 
 function Dashboard() {
+  const [auth] = useAuth();
   return (
     <Layout>
-      <h1>Dashboard</h1>
+      <div className="container-fluid m-3 p-3">
+        <div className="row">
+          <div className="col-md-3">
+            <UserMenu />
+          </div>
+          <div className="col-md-9">
+            <div className="card w-75 p-3">
+              <h3>Name : {auth?.user?.name}</h3>
+              <h3>Email : {auth?.user?.email}</h3>
+              <h3>Address : {auth?.user?.address}</h3>
+            </div>
+          </div>
+        </div>
+      </div>
     </Layout>
   );
 }
